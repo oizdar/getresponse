f7016244689fbe54e0cc54f45e48c58e43ff711c
Radosław Malinowski <malinowski.rad@gmail.com>
2016-10-14 10:41:58 +0200

    change tabs on spaces

--- GetResponseApi.php
+++ GetResponseApi.php
@@ -1,78 +1,77 @@
-<?php
+    <?php
 
-class GetResponseApi
-{
-	/**
-	 * GetResponse autentication parameters
-	 * @var  string $apiKey
-	 * @var  string $apiUrl
-	 * @var  string $domain 	customer domain (enterprise account users)
-	 */
-	private $apiKey;
-	private $apiUrl = 'https://api.getresponse.com/v3';
-	private $domain = null;
+    class GetResponseApi
+    {
+    /**
+     * GetResponse autentication parameters
+     * @var  string $apiKey
+     * @var  string $apiUrl
+     * @var  string $domain 	customer domain (enterprise account users)
+     */
+    private $apiKey;
+    private $apiUrl = 'https://api.getresponse.com/v3';
+    private $domain = null;
 
-	/**
-	 * Http Status code last request
-	 * @var int
-	 */
-	private $httpStatus;
+    /**
+     * Http Status code last request
+     * @var int
+     */
+    private $httpStatus;
 
-	/**
-	 * Array of JSON encoded responses
- 	 * @var array of strings
- 	 */
-	private $response;
+    /**
+     * Array of JSON encoded responses
+     * @var array of strings
+     */
+    private $response;
 
-/**
- * @param string      $apiKey GetResponse acoount Api-Key
- * @param string|null $apiUrl Change only for Enterprise accounts otherwise use default
- * @param string|null $domain Enterprise accounts only
- */
-	public function __construct(
-		string $apiKey,
-		string $apiUrl = null,
-		string $domain = null
-	) {
-		$this->apiKey = $apiKey;
+    /**
+    * @param string      $apiKey GetResponse acoount Api-Key
+    * @param string|null $apiUrl Change only for Enterprise accounts otherwise use default
+    * @param string|null $domain Enterprise accounts only
+    */
+    public function __construct(
+        string $apiKey,
+        string $apiUrl = null,
+        string $domain = null
+        ) {
+            $this->apiKey = $apiKey;
 
-		if (!empty($apiUrl)) {
-			$this->apiUrl = $apiUrl;
-		}
-		if (!empty($domain)) {
-			$this->domain = $domain;
-		}
-	}
+        if (!empty($apiUrl)) {
+            $this->apiUrl = $apiUrl;
+        }
+        if (!empty($domain)) {
+            $this->domain = $domain;
+        }
+    }
 
-	public function ping()
-	{
-		$this->getAccounts();
-		return ($this->httpStatus === 200) ? true : false;
+    public function ping()
+    {
+        $this->getAccounts();
+        return ($this->httpStatus === 200) ? true : false;
+    }
 
-	}
+    public function getAccounts()
+    {
+        $this->call('accounts');
+        return $this;
+    }
 
-	public function getAccounts()
-	{
-		$this->call('accounts');
-		return $this;
-	}
-
-	/**
-	 * cURL Call
-	 * Sets object values with received data (::httpStatus, ::response)
-	 *
-	 * @param  string $method     GetResponse method
-	 * @param  string $httpMethod
-	 * @param  array  $params     GetResponse params
-	 * @return void
-	 */
-	private function call(
-		string $method,
-		string $httpMethod = 'GET',
-		array $params = []
-	) {
-		if (empty($method)) {
-			$this->httpStatus = 400;
+    /**
+     * cURL Call
+     * Sets object values with received data (::httpStatus, ::response)
+     *
+     * @param  string $method     GetResponse method
+     * @param  string $httpMethod
+     * @param  array  $params     GetResponse params
+     * @return void
+     */
+    private function call(
+        string $method,
+        string $httpMethod = 'GET',
+        array $params = []
+    ) {
+        if (empty($method)) {
+            $this->httpStatus = 400;
             return [
                 'httpStatus' => '400',
                 'code' => '1010',
@@ -82,49 +81,49 @@ class GetResponseApi
         }
 
         $url = $this->apiUrl . '/' . $method;
-		$options = [
-			CURLOPT_URL => $url,
-			CURLOPT_HEADER => false,
+        $options = [
+            CURLOPT_URL => $url,
+            CURLOPT_HEADER => false,
             CURLOPT_HTTPHEADER => ['X-Auth-Token: api-key ' . $this->apiKey, 'Content-Type: application/json'],
             CURLOPT_RETURNTRANSFER => true,
 
-		];
+        ];
 
-		if ($httpMethod === 'POST') {
+        if ($httpMethod === 'POST') {
             $options[CURLOPT_POST] = 1;
             $options[CURLOPT_POSTFIELDS] = $params;
         } else if ($httpMethod === 'DELETE') {
             $options[CURLOPT_CUSTOMREQUEST] = 'DELETE';
         }
 
-		$resource = curl_init();
-		curl_setopt_array($resource, $options);
+        $resource = curl_init();
+        curl_setopt_array($resource, $options);
 
-		$response = curl_exec($resource);
-		$this->httpStatus = curl_getinfo($resource, CURLINFO_HTTP_CODE);
+        $response = curl_exec($resource);
+        $this->httpStatus = curl_getinfo($resource, CURLINFO_HTTP_CODE);
 
-		curl_close($resource);
+        curl_close($resource);
 
-		$this->response = $response;
-	}
+        $this->response = $response;
+    }
 
-	/**
-	 * Returns last call Response
-	 *
-	 * @param  boolean $object  	For true returns splObject otherwise array
-	 * @return stdClass|array 	Response as array or object
-	 */
-	public function returnResponse(bool $object = true)
-	{
-		if($object) {
-			$response = (isset($this->response))
-				? json_decode($this->response)
-				: new StdClass();
-		} else {
-			$response = (isset($this->response))
-				? json_decode($this->response, true)
-				: array();
-		}
-		return $response;
-	}
+    /**
+     * Returns last call Response
+     *
+     * @param  boolean $object  	For true returns splObject otherwise array
+     * @return stdClass|array 	Response as array or object
+     */
+    public function returnResponse(bool $object = true)
+    {
+        if($object) {
+            $response = (isset($this->response))
+                ? json_decode($this->response)
+                : new StdClass();
+        } else {
+            $response = (isset($this->response))
+                ? json_decode($this->response, true)
+                : array();
+        }
+        return $response;
+    }
 }
--- GetResponseTest.php
+++ GetResponseTest.php
@@ -9,47 +9,45 @@ use \Math\Matrix;
 
 class GetResponseTest extends \PHPUnit_Framework_TestCase
 {
-	/**
-	 * configured object
-	 *
-	 * @var GetResponseApi
-	 */
-	private $getresponse;
+    /**
+    * configured object
+    *
+    * @var GetResponseApi
+    */
+    private $getresponse;
 
-	public function setUp()
-	{
-		$this->getresponse = new GetResponseApi(API_KEY);
-	}
+    public function setUp()
+    {
+        $this->getresponse = new GetResponseApi(API_KEY);
+    }
 
-	public function testConnection()
-	{
-		$this->assertEquals(true, $this->getresponse->ping());
-		$this->getresponse = new GetResponseApi('APIKEY');
-		$this->assertEquals(false, $this->getresponse->ping());
-	}
+    public function testConnection()
+    {
+        $this->assertEquals(true, $this->getresponse->ping());
+        $this->getresponse = new GetResponseApi('APIKEY');
+        $this->assertEquals(false, $this->getresponse->ping());
+    }
 
-	public function testGetAccounts()
-	{
-		$responseObject = $this->getresponse->getAccounts();
-		$this->assertInstanceOf('GetResponseApi', $responseObject);
-		$this->assertSame($this->getresponse, $responseObject);
+    public function testGetAccounts()
+    {
+        $responseObject = $this->getresponse->getAccounts();
+        $this->assertInstanceOf('GetResponseApi', $responseObject);
+        $this->assertSame($this->getresponse, $responseObject);
+    }
 
-	}
+    public function testReturnResponse()
+    {
+        $this->getresponse->getAccounts();
 
-	public function testReturnResponse()
-	{
-		$this->getresponse->getAccounts();
+        $object = $this->getresponse->returnResponse();
+        $this->assertInstanceOf('StdClass', $object);
+        $this->assertTrue(isset($object->accountId));
 
-		$object = $this->getresponse->returnResponse();
-		$this->assertInstanceOf('StdClass', $object);
-		$this->assertTrue(isset($object->accountId));
+        $array = $this->getresponse->returnResponse(false);
+        $this->assertTrue(is_array($array));
+        $this->assertFalse(empty($array));
+        $this->assertTrue(isset($array['accountId']));
 
-		$array = $this->getresponse->returnResponse(false);
-		$this->assertTrue(is_array($array));
-		$this->assertFalse(empty($array));
-		$this->assertTrue(isset($array['accountId']));
-
-		$this->assertEquals($array['accountId'], $object->accountId);
-	}
-
+        $this->assertEquals($array['accountId'], $object->accountId);
+    }
 }
