6792b9b6235dc9bfb537df88ad3358b6d80f4dff
Radosław Malinowski <malinowski.rad@gmail.com>
2016-10-15 16:39:25 +0200

    Created methods: add/getCampaigns

--- GetResponseApi.php
+++ GetResponseApi.php
@@ -53,8 +53,41 @@
     public function getAccounts()
     {
         $this->call('accounts');
-        return $this;
     }
+
+    /**
+     * Adding Account
+     *
+     * @param array $params Map of getResponse parameters: Possible options:
+     *    firstName    string  First name, length 2-64
+     *    lastName    string  Last name, length 2-64
+     *    phone   string  Phone, length 2-32
+     *    companyName string  Company name, length 0-64
+     *    street  string  Street, length 2-64
+     *    state   string  State, length 0-40
+     *    city    string  City
+     *    zipCode string  zip code, length 2-9
+     *    timeFormat  string  Time format (Fixed values to choose from: 12h, 24h)
+     *    numberOfEmployees   string  Number of employees in Your company
+     *    industryTag string  id of industry tag
+     *
+     * @todo  Check and repair
+     */
+    public function addAccount(array $params)
+    {
+        //$this->call('accounts', 'POST', $params);
+    }
+
+    public function addCampaign($params)
+    {
+        $this->call('campaigns', 'POST', $params);
+    }
+
+    public function getCampaigns()
+    {
+        $this->call('campaigns');
+    }
+
 
     /**
      * cURL Call
@@ -64,6 +97,8 @@
      * @param  string $httpMethod
      * @param  array  $params     GetResponse params
      * @return void
+     *
+     * @todo  set Params to GET query
      */
     private function call(
         string $method,
@@ -90,9 +125,13 @@
         ];
 
         if ($httpMethod === 'POST') {
-            $options[CURLOPT_POST] = 1;
-            $options[CURLOPT_POSTFIELDS] = $params;
-        } else if ($httpMethod === 'DELETE') {
+            $options[CURLOPT_POST] = true;
+            $json = json_encode($params);
+            var_dump($json);
+            $options[CURLOPT_POSTFIELDS] = $json;
+        } elseif ($httpMethod === 'GET') {
+            // set query with params
+        } elseif ($httpMethod === 'DELETE') {
             $options[CURLOPT_CUSTOMREQUEST] = 'DELETE';
         }
 
@@ -105,6 +144,14 @@
         curl_close($resource);
 
         $this->response = $response;
+    }
+
+    /**
+     * @return integer|boolean
+     */
+    public function getHttpStatus()
+    {
+        return (isset($this->httpStatus)) ? $this->httpStatus : false;
     }
 
     /**
--- GetResponseTest.php
+++ GetResponseTest.php
@@ -20,19 +20,15 @@ class GetResponseTest extends \PHPUnit_Framework_TestCase
     {
         $this->getresponse = new GetResponseApi(API_KEY);
     }
-
-    public function testConnection()
+    public function testEmptyHttpStatus()
+    {
+        $this->assertFalse(false, $this->getresponse->getHttpStatus());
+    }
+    public function testPing()
     {
         $this->assertEquals(true, $this->getresponse->ping());
         $this->getresponse = new GetResponseApi('APIKEY');
         $this->assertEquals(false, $this->getresponse->ping());
-    }
-
-    public function testGetAccounts()
-    {
-        $responseObject = $this->getresponse->getAccounts();
-        $this->assertInstanceOf('GetResponseApi', $responseObject);
-        $this->assertSame($this->getresponse, $responseObject);
     }
 
     public function testReturnResponse()
@@ -49,5 +45,53 @@ class GetResponseTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue(isset($array['accountId']));
 
         $this->assertEquals($array['accountId'], $object->accountId);
+    }
+
+    /**
+     * Doesn't work ?? UNPARSABLE JSON BODY?
+     *
+    {
+        $accountValues = [
+            'firstName'=> 'John',
+            'lastName'=> 'Smith',
+            'companyName'=> 'CELAR',
+        ];
+        $this->getresponse->addAccount($accountValues);
+        $object = $this->getresponse->returnResponse();
+        var_dump($object);
+        $this->assertEquals(200, $this->getresponse->getHttpStatus());
+
+    }*/
+
+    public function testGetAccounts()
+    {
+        $responseObject = $this->getresponse->getAccounts();
+        $this->assertEquals(200, $this->getresponse->getHttpStatus());
+    }
+
+    /**
+     * Tests Adding Campaign. Cannot remove campaign by API, Can you do this only
+     * on GetResponse Website. In Account Settings. Uncoment only when sure.
+     *
+    public function testAddCampaign()
+    {
+        $params = [
+            'name' => 'test_campaig_test',
+            'languageCode' => 'PL',
+            'profile' => [
+                'description' => 'Short Campaign description',
+                'title' => 'Campaign Title'
+            ]
+        ];
+
+        $this->getresponse->addCampaign($params);
+        $this->assertEquals(201, $this->getresponse->getHttpStatus());
+
+    }*/
+    public function testGetCampaigns()
+    {
+        $this->getresponse->getCampaigns();
+        $this->assertEquals(200, $this->getresponse->getHttpStatus());
+        $this->assertGreaterThan(0, count($this->getresponse->returnResponse()));
     }
 }
